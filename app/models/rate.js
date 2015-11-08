6f479264cd76a9340bbe74651cd9d863e74180fd
Michael M Kuhinica <taverneiro@gmail.com>
2015-11-08 17:26:24 +1000

    Removed rate model

--- app/models/rate.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  ask: DS.attr('number'),
-  bid: DS.attr('number'),
-  last: DS.attr('number'),
-  exchange: DS.belongsTo('exchange')
-});
