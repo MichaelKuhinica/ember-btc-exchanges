4ad176f1812beb09f7b02d005433750a6742fbb4
Michael M Kuhinica <taverneiro@gmail.com>
2015-11-08 14:13:51 +1000

    Find exchange on index route and present it

--- app/routes/rates/index.js
+++ app/routes/rates/index.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return Ember.$.getJSON('https://api.bitcoinaverage.com/exchanges/USD');
+    return this.store.findAll('exchange');
   }
 });
