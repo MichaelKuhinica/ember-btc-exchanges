16fcdcf4e7e8c0bcfca3e8576b11d4b5fba9dfb6
Michael M Kuhinica <taverneiro@gmail.com>
2015-11-10 09:26:11 +1000

    Helper to show model price according to direction flag (sell/buy)

---
+++ tests/unit/helpers/directional-price-test.js
@@ -0,0 +1,10 @@
+import { directionalPrice } from '../../../helpers/directional-price';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | directional price');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = directionalPrice(42);
+  assert.ok(result);
+});
