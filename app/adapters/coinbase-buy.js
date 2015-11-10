e67f3cb21871bd2bcd3c4b9d15f90413c2cac6ba
Michael M Kuhinica <taverneiro@gmail.com>
2015-11-10 12:34:16 +1000

    coinable refactored to fit sell

--- app/adapters/coinbase-buy.js
+++ app/adapters/coinbase-buy.js
@@ -1,27 +1,9 @@
 import CoinbaseAdapter from './coinbase';
-import Ember from 'ember';
 
 export default CoinbaseAdapter.extend({
   createRecord: function(store, type, snapshot) {
     var client = this.client(snapshot);
-    var args = {
-      "amount": snapshot.get("amount"),
-      "currency": "BTC"
-    };
-    return new Ember.RSVP.Promise(function(resolve, reject) {
-      client.getAccounts({}, function(err, accounts) {
-        if(err) {
-          Ember.run(null, reject, err);
-        } else if(accounts.length > 0) {
-          accounts[0].buy(args, function(err, xfer) {
-            if(err) {
-              Ember.run(null, reject, err);
-            } else {
-              Ember.run(null, resolve, xfer);
-            }
-          });
-        }
-      });
-    });
+    var args = this.orderRequestArgs(snapshot);
+    return this.performOrder(client, 'buy', args);
   }
 });
--- app/adapters/coinbase.js
+++ app/adapters/coinbase.js
@@ -1,5 +1,6 @@
 import ExchangeAdapter from './exchange';
 import coinbase from 'npm:coinbase';
+import Ember from 'ember';
 
 export default ExchangeAdapter.extend({
   client: function(snapshot) {
@@ -13,5 +14,40 @@ export default ExchangeAdapter.extend({
     };
     var client = new Client(clientArgs);
     return client;
+  },
+  orderRequestArgs: function(snapshot) {
+    return {
+      "amount": snapshot.get("amount"),
+      "currency": "BTC"
+    };
+  },
+  performOrder: function(client, direction, args) {
+    var that = this;
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      that.getAccount(client).then(function(data) {
+        data[direction](args, function(err, xfer) {
+          if(err) {
+            Ember.run(null, reject, err);
+          } else {
+            Ember.run(null, resolve, xfer);
+          }
+        });
+      }, function(err) {
+          Ember.run(null, reject, err);
+      });
+    });
+  },
+  getAccount: function(client) {
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      client.getAccounts({}, function(err, accounts) {
+        if(err) {
+          Ember.run(null, reject, err);
+        } else if(accounts.length > 0) {
+          Ember.run(null, resolve, accounts[0]);
+        } else {
+          Ember.run(null, reject, err);
+        }
+      });
+    });
   }
 });
