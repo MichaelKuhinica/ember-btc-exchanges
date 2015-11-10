3499127ad70c67b744eee8ede949857a0adef068
Michael M Kuhinica <taverneiro@gmail.com>
2015-11-10 13:46:02 +1000

    Bitex buy and sell implementations

---
+++ app/adapters/bitex-buy.js
@@ -0,0 +1,8 @@
+import BitexAdapter from './bitex';
+
+export default BitexAdapter.extend({
+  createRecord: function(store, type, snapshot) {
+    var data = this.createData(snapshot);
+    return this.performRequest('POST', 'private/bids', data);
+  }
+});
