28c98c36c13780e24bc3c429bc432380e3b43c23
Michael M Kuhinica <taverneiro@gmail.com>
2015-11-10 11:37:08 +1000

    Extracted amount fields parameter

--- app/components/amount-field.js
+++ app/components/amount-field.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    orderBtc: function(amount, m, direction) {
+      this.sendAction('action', amount, m, direction);
+    }
+  }
 });
