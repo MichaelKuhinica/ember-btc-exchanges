a17085947a26da1c7bfed985c06d00691d8257c8
Michael M Kuhinica <taverneiro@gmail.com>
2015-11-10 12:34:46 +1000

    Coinbase sell adapter and models

---
+++ app/adapters/coinbase-sell.js
@@ -0,0 +1,9 @@
+import CoinbaseAdapter from './coinbase';
+
+export default CoinbaseAdapter.extend({
+  createRecord: function(store, type, snapshot) {
+    var client = this.client(snapshot);
+    var args = this.orderRequestArgs(snapshot);
+    return this.performOrder(client, 'sell', args);
+  }
+});
