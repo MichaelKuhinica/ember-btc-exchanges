3ceae5068c0efd97248a8680b511bc0147733207
Michael M Kuhinica <taverneiro@gmail.com>
2015-11-10 11:36:29 +1000

    Extracted model loading to abstract route

--- app/routes/rates/buy.js
+++ app/routes/rates/buy.js
@@ -1,11 +1,5 @@
-import Ember from 'ember';
+import OrderRoute from './order';
 
-export default Ember.Route.extend({
-  model(params) {
-    if(this.store.hasRecordForId('rate', params.exchange)) {
-      return this.store.find('rate', params.exchange);
-    }
-    this.transitionTo('rates.index');
-  },
-  amount: 0
+export default OrderRoute.extend({
+
 });
